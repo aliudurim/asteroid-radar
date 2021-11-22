854f8bfd1c9bf0be7f712b1ee06b5fcc06ba0136
DA <durim.aliu5@gmail.com>
2021-11-22 15:12:54 +0100

    Create new package.

--- app/src/main/java/com/udacity/asteroidradar/ui/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.udacity.asteroidradar
+package com.udacity.asteroidradar.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udacity.asteroidradar.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
