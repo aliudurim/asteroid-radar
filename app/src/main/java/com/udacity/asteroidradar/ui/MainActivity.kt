3deb07d88223e5fe2113f30954d16e6b5c84ab1a
DA <durim.aliu5@gmail.com>
2021-11-22 15:27:31 +0100

    Create nav file.

--- app/src/main/java/com/udacity/asteroidradar/ui/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/MainActivity.kt
@@ -2,11 +2,34 @@ package com.udacity.asteroidradar.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment)
+            .navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        setSupportActionBar(binding.toolBar)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment,
+                R.id.detailFragment
+            )
+        )
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
